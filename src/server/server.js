cf84def1cce04651cf34b373fc28e3c585b8d59f
root <root@BETELNOTE658.beteltecnologia.net.br>
2023-06-23 17:32:38 -0300

    commit inicial

--- src/server/server.js
+++ src/server/server.js
@@ -17,26 +17,26 @@ db.connect(function (err) {
 });
 
 // LISTAR
-app.get("/api/bebidas/listar", (req, res) => {
+app.get("/api/professores/listar", (req, res) => {
     const { busca } = req.query;
 
     if (busca || busca != null) {
-        const query = "SELECT * FROM bebidas WHERE nome LIKE ?";
+        const query = "SELECT * FROM professores WHERE nome LIKE ?";
         const search = `%${busca}%`;
 
         db.query(query, [search], (err, result) => {
             if (err) {
                 console.log(err);
-                res.status(500).send("Erro ao buscar bebidas");
+                res.status(500).send("Erro ao buscar professores");
             } else {
                 res.send(result);
             }
         });
     } else {
-        db.query("SELECT * FROM bebidas", (err, result) => {
+        db.query("SELECT * FROM professores", (err, result) => {
             if (err) {
                 console.log(err);
-                res.status(500).send("Erro ao listar bebidas");
+                res.status(500).send("Erro ao listar professores");
             } else {
                 res.send(result);
             }
@@ -45,73 +45,40 @@ app.get("/api/bebidas/listar", (req, res) => {
 });
 
 
-// ADICIONAR
-app.get('/api/bebidas/adicionar', (req, res) => {
-    Promise.all([
-        new Promise((resolve, reject) => {
-            db.query("SELECT * FROM categorias", (err, result) => {
-                if (err) {
-                    console.log(err);
-                    reject("Erro ao listar categorias");
-                } else {
-                    resolve(result);
-                }
-            });
-        })
-    ])
-        .then(([categorias]) => {
-            res.json({ categorias });
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).send("Erro ao obter os dados");
-        });
-});
+app.post('/api/professores/adicionar', (req, res) => {
+    const { professor } = req.body;
+    const { nome, especialidade } = professor;
 
-app.post('/api/bebidas/adicionar', (req, res) => {
-    const { bebida } = req.body;
-    const { nome, descricao, categoria_id, teor_alcoolico, categoria_nome } = bebida;
-
-    const query = "INSERT INTO bebidas (nome, descricao, categoria_id, teor_alcoolico, categoria_nome) VALUES (?, ?, ?, ?, ?)";
-    db.query(query, [nome, descricao, categoria_id, teor_alcoolico, categoria_nome], (err, result) => {
+    const query = "INSERT INTO professores (nome, especialidade) VALUES (?, ?)";
+    db.query(query, [nome, especialidade], (err, result) => {
         if (err) {
             console.log(err);
-            res.json({ status: "error", message: "Não foi possível cadastrar a bebida!" });
+            res.json({ status: "error", message: "Não foi possível cadastrar o professor!" });
         } else {
-            res.json({ status: "success", message: "Bebida adicionada com sucesso!" });
+            res.json({ status: "success", message: "Professor adicionado com sucesso!" });
         }
     });
 });
 
 //EDITAR
-app.get('/api/bebidas/editar/:id', (req, res) => {
+app.get('/api/professores/editar/:id', (req, res) => {
     Promise.all([
-        new Promise((resolve, reject) => {
-            db.query("SELECT * FROM categorias", (err, result) => {
-                if (err) {
-                    console.log(err);
-                    reject("Erro ao listar categorias");
-                } else {
-                    resolve(result);
-                }
-            });
-        }),
         new Promise((resolve, reject) => {
             const { id } = req.params;
             console.log(id)
-            const query = "SELECT * FROM bebidas WHERE id = ?";
+            const query = "SELECT * FROM professores WHERE id = ?";
             db.query(query, [id], (err, result) => {
                 if (err) {
                     console.log(err);
-                    reject("Erro ao buscar bebida");
+                    reject("Erro ao buscar professor");
                 } else {
                     resolve(result[0]);
                 }
             });
         })
     ])
-        .then(([categorias, bebida]) => {
-            res.json({ categorias, bebida });
+        .then((professor) => {
+            res.json(professor);
         })
         .catch(error => {
             console.log(error);
@@ -119,12 +86,11 @@ app.get('/api/bebidas/editar/:id', (req, res) => {
         });
 });
 
-app.put('/api/bebidas/editar/:id', (req, res) => {
+app.put('/api/professores/editar/:id', (req, res) => {
     const { id } = req.params;
-    console.log(req.body);
-    const { nome, descricao, categoria_id, categoria_nome, teor_alcoolico } = req.body;
-    const query = "UPDATE bebidas SET nome = ?, descricao = ?, categoria_id = ?, categoria_nome = ?, teor_alcoolico = ? WHERE id = ?";
-    db.query(query, [nome, descricao, categoria_id, categoria_nome, teor_alcoolico, id], (err, result) => {
+    const { nome, especialidade } = req.body;
+    const query = "UPDATE professores SET nome = ?, especialidade = ? WHERE id = ?";
+    db.query(query, [nome, especialidade, id], (err, result) => {
         if (err) {
             console.log(err);
             res.json({ status: "error", message: "Não foi possível editar a bebida!" });
@@ -135,9 +101,9 @@ app.put('/api/bebidas/editar/:id', (req, res) => {
 });
 
 // VISUALIZAR
-app.get('/api/bebidas/visualizar/:id', (req, res) => {
+app.get('/api/professores/visualizar/:id', (req, res) => {
     const { id } = req.params;
-    const query = "SELECT * FROM bebidas WHERE id = ?";
+    const query = "SELECT * FROM professores WHERE id = ?";
     db.query(query, [id], (err, result) => {
         if (err) {
             console.log(err);
